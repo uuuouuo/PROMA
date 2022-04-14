3ba852c9a1c8946ce0d48018dcc06664fbf4a9ce
최지수 <cjscoding1997@gmail.com>
2022-04-14 13:59:15 +0900

    [S06P31C107-69]feat: styled-components 환경 설정

--- frontend/proma/pages/_app.tsx
+++ frontend/proma/pages/_app.tsx
@@ -1,13 +1,33 @@
 /* eslint-disable */
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React, { useState, useEffect } from "react";
 import wrapper from "../store/configureStore";
-import { useSelector } from "react-redux";
-import { RootState } from "../store/modules";
+
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
---
+++ frontend/proma/pages/workspace/project/test.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+  color: white;
+`;
+
+const Test = () => {
+  return (
+    <>
+      <Box>Test</Box>
+    </>
+  );
+};
+
+export default Test;
