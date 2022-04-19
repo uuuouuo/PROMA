ab6eaca7766207b4e93c3fb1b442e2b27fc4e096
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-19 10:23:15 +0900

    [S06P31C107-105] feat: 로그인 모달 생성

--- frontend/proma/components/common/NavBar.tsx
+++ frontend/proma/components/common/NavBar.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import styled from "styled-components";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 const Nav_bar = styled.div`
   height: 15vh;
@@ -31,7 +34,33 @@ const Memberfunc = styled.div`
   align-self: center;
 `;
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const Login = styled.div`
+  height: 50px;
+  justify-content: center;
+  background: lightgrey;
+  font-weight: bold;
+  font-size: 23px;
+  display: flex;
+  align-items: center;
+`;
+
 const NavBar = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Nav_bar>
@@ -45,8 +74,21 @@ const NavBar = () => {
           </Profileimg>
         </Profile>
         <Memberfunc>
-          <a>로그인 / 회원가입</a>
+          <a onClick={handleOpen}>로그인 / 회원가입</a>
         </Memberfunc>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Login>
+              Github
+            </Login>
+          </Box>
+        </Modal>
       </Nav_bar>
     </>
   );
