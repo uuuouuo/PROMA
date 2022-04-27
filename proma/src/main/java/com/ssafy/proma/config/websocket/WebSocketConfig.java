8d278429d2ea0e1e2d604ccdc5ecac1e6d446c21
장소명 <somyeong3@naver.com>
2022-04-27 17:50:34 +0900

    [S06P31C107-213] fix: user 부분 수정

--- proma/src/main/java/com/ssafy/proma/config/websocket/WebSocketConfig.java
+++ proma/src/main/java/com/ssafy/proma/config/websocket/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.ssafy.proma.config.websocket;
 
-import com.ssafy.proma.config.websocket.WebsocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
