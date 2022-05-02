75e29c3a249a4051e00cbb3d4098a674e60218f6
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-02 16:51:49 +0900

    [S06P31C107-244] chore: 로그인 코드 수정 중(OAuth 로그인 구현)

--- frontend/proma/pages/user/login/github/callback.tsx
+++ frontend/proma/pages/user/login/github/callback.tsx
@@ -1,17 +1,18 @@
 import { useEffect } from 'react';
 import { BsChevronCompactLeft } from 'react-icons/bs';
-import { getMemberList } from "../../../../store/modules/member";
+import { getMemberList, getMemberInfo } from "../../../../store/modules/member";
 
 const Callback = () => {
 
     useEffect(() => {
         const code = window.location.search.replace("?code=", "");
         localStorage.setItem("code", code);
-        getMemberList();
-        console.log(code);
-        
-    }, [])
+        getMemberList().then(() => Log_in());
+    }, []);
 
+    const Log_in = () => {
+        getMemberInfo();
+    }
     return (
         <></>
     );
--- frontend/proma/store/modules/member.ts
+++ frontend/proma/store/modules/member.ts
@@ -10,42 +10,54 @@ const initialState: MemberState = {
     memberList: [],
 }
 
-export const getMemberList = createAsyncThunk(
-    "GET/USER/LOGIN/GITHUB",
-    async (_, { rejectWithValue }) => {
-        // return await axios
-        //     .get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=`, code)
-        //     // .then((res) => res.data)
-        //     .then((res) => console.log(res))
+// export const getMemberList = createAsyncThunk(
+//     "GET/USER/LOGIN/GITHUB",
+//     async (_, { rejectWithValue }) => {
+//         // return await axios
+//         //     .get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=`, code)
+//         //     // .then((res) => res.data)
+//         //     .then((res) => console.log(res))
 
-        // const code = localStorage.getItem("code");
-        // const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`).then((res) => console.log(res))
-        // console.log("된다");
+//         // const code = localStorage.getItem("code");
+//         // const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`).then((res) => console.log(res))
+//         // console.log("된다");
 
-        const code = localStorage.getItem("code");
+//         const code = localStorage.getItem("code");
 
-        // return await axios
-        // .get(`${BACKEND_URL}/user/login/github?code=`, {
-        //     headers: {
-        //     code: `${code}`,
-        //     },
-        // })
-        // .then((res) => res.data)
-        // .catch((err) => rejectWithValue(err.response.data));
+//         // return await axios
+//         // .get(`${BACKEND_URL}/user/login/github?code=`, {
+//         //     headers: {
+//         //     code: `${code}`,
+//         //     },
+//         // })
+//         // .then((res) => res.data)
+//         // .catch((err) => rejectWithValue(err.response.data));
 
-        try {
-            const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`);
+//         try {
+//             const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`);
 
-            if (response.status !== 200) {
-                throw new Error(response.statusText);
-            }
-            return response.data;
+//             if (response.status !== 200) {
+//                 throw new Error(response.statusText);
+//             }
+//             return response.data;
 
-        } catch (error) {
-            console.log("에러");
-        }
-    }
-);
+//         } catch (error) {
+//             console.log("에러");
+//         }
+//     }
+// );
+
+export const getMemberList = async () => {
+    const code = localStorage.getItem("code");
+
+    return await axios.get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=${code}`)
+        .then((res) => localStorage.setItem("jwtToken", res.data.jwtToken));
+}
+
+export const getMemberInfo = async () => {
+    return await axios.get(`http://k6c107.p.ssafy.io:8080/user/data`)
+        .then((res) => console.log(res));
+}
 
 const memberSlice = createSlice({
     name: "member",
@@ -53,11 +65,11 @@ const memberSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(getMemberList.pending, (state) => { })
-            .addCase(getMemberList.fulfilled, (state, { payload }) => {
-                state.memberList = payload;
-            })
-            .addCase(getMemberList.rejected, (state) => { });
+            // .addCase(getMemberList.pending, (state) => { })
+            // .addCase(getMemberList.fulfilled, (state, { payload }) => {
+            //     state.memberList = payload;
+            // })
+            // .addCase(getMemberList.rejected, (state) => { });
     },
 });
 
