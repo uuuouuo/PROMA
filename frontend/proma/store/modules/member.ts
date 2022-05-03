45762b1b29903878270600a12df3d8f272712aec
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 14:34:23 +0900

    [S06P31C107-248] chore: 사용자 정보 가져오기 구현

--- frontend/proma/store/modules/member.ts
+++ frontend/proma/store/modules/member.ts
@@ -2,74 +2,61 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { BACKEND_URL } from "../../config";
 import axios from "axios";
 
-export type MemberState = {
-    memberList: any;
+export type UserState = {
+    userInfo: Array<Object>;
+    isLogin: boolean;
 };
 
-const initialState: MemberState = {
-    memberList: [],
+const initialState: UserState = {
+    userInfo: [],
+    isLogin: false,
 }
 
-// export const getMemberList = createAsyncThunk(
-//     "GET/USER/LOGIN/GITHUB",
-//     async (_, { rejectWithValue }) => {
-//         // return await axios
-//         //     .get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=`, code)
-//         //     // .then((res) => res.data)
-//         //     .then((res) => console.log(res))
+export const getUserInfo = createAsyncThunk(
+    "USER/DATA",
+    async (_, { rejectWithValue }) => {
 
-//         // const code = localStorage.getItem("code");
-//         // const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`).then((res) => console.log(res))
-//         // console.log("된다");
+        const jwtToken = localStorage.getItem("jwtToken");
+        return await axios
+            .get(`http://k6c107.p.ssafy.io:8080/user/data`, {
+                headers: {
+                    JWT: `Bearer ${jwtToken}`
+                },
+        })
+        .then((res) => res.data)
+        .catch((err) => rejectWithValue(err.response.data));
+    }
+);
 
-//         const code = localStorage.getItem("code");
-
-//         // return await axios
-//         // .get(`${BACKEND_URL}/user/login/github?code=`, {
-//         //     headers: {
-//         //     code: `${code}`,
-//         //     },
-//         // })
-//         // .then((res) => res.data)
-//         // .catch((err) => rejectWithValue(err.response.data));
-
-//         try {
-//             const response = await axios.get(`${BACKEND_URL}/user/login/github?code=${code}`);
-
-//             if (response.status !== 200) {
-//                 throw new Error(response.statusText);
-//             }
-//             return response.data;
-
-//         } catch (error) {
-//             console.log("에러");
-//         }
-//     }
-// );
-
-export const getMemberList = async () => {
-    const code = localStorage.getItem("code");
-
-    return await axios.get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=${code}`)
-        .then((res) => localStorage.setItem("jwtToken", res.data.jwtToken));
-}
-
-export const getMemberInfo = async () => {
-    return await axios.get(`http://k6c107.p.ssafy.io:8080/user/data`)
-        .then((res) => console.log(res));
-}
+export const getLogin = createAsyncThunk(
+    "USER/LOGIN/GITHUB",
+    async (_, thunkAPI) => {
+        const code = localStorage.getItem("code");
+        return await axios
+            .get(`http://k6c107.p.ssafy.io:8080/user/login/github?code=${code}`)
+            .then((res) => {
+                localStorage.setItem("jwtToken", res.data.jwtToken);
+                thunkAPI.dispatch(getUserInfo());
+    })
+        .catch((err) => thunkAPI.rejectWithValue(err.response.data));
+    }
+);
 
 const memberSlice = createSlice({
     name: "member",
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder
-            // .addCase(getMemberList.pending, (state) => { })
-            // .addCase(getMemberList.fulfilled, (state, { payload }) => {
-            //     state.memberList = payload;
-            // })
-            // .addCase(getMemberList.rejected, (state) => { });
+        builder.addCase(
+            getUserInfo.fulfilled,
+            (state, { payload }: { payload: { userInfo: any } }) => {
+                state.userInfo = payload.userInfo;
+            }
+        ).addCase(
+            getLogin.fulfilled, (state) => {
+                state.isLogin = true;
+            }
+        )
     },
 });
 
