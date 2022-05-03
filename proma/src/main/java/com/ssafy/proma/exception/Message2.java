06153aa2c1e443bb8b420e23d75c3fce5a0212c6
장소명 <somyeong3@naver.com>
2022-05-03 15:32:59 +0900

    [S06P31C107-256] refactor: chat message 추가 및 controller, service 수정

---
+++ proma/src/main/java/com/ssafy/proma/exception/Message2.java
@@ -0,0 +1,16 @@
+package com.ssafy.proma.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Message2 {
+
+  // chat
+  public static final String PRIVATE_CHATROOM_SUCCESS_MESSAGE = "팀 채팅방 생성 성공";
+  public static final String PRIVATE_CHATROOM_ERROR_MESSAGE = "팀 채팅방 생성 실패";
+  public static final String TEAM_CHATROOM_SUCCESS_MESSAGE = "팀 채팅방 생성 성공";
+  public static final String TEAM_CHATROOM_ERROR_MESSAGE = "팀 채팅방 생성 실패";
+  public static final String PROJECT_CHATROOM_SUCCESS_MESSAGE = "팀 채팅방 생성 성공";
+  public static final String PROJECT_CHATROOM_ERROR_MESSAGE = "팀 채팅방 생성 실패";
+
+}
