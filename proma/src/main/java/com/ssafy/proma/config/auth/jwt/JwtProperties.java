9f0e076942a0c9f7f680b9c593f7d40afc8079e3
김일환 <ikhwan62@naver.com>
2022-05-04 15:54:23 +0900

    [S06P31C107] fix: JWT expiration 시간 수정

--- proma/src/main/java/com/ssafy/proma/config/auth/jwt/JwtProperties.java
+++ proma/src/main/java/com/ssafy/proma/config/auth/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.ssafy.proma.config.auth.jwt;
 
 public interface JwtProperties {
     String SECRET = "proma";
-    int JWT_EXPIRATION_TIME = 1000 * 60 * 60 * 10;
+    int JWT_EXPIRATION_TIME = 1000 * 30;
     int REF_EXPIRATION_TIME = 1000 * 60 * 60 * 6;
     String TOKEN_PREFIX = "Bearer ";
     String JWT_HEADER_STRING = "Authorization";
