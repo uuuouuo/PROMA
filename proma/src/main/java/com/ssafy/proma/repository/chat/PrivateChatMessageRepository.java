af570981913b8feb8d69a65b02d0c401326691e9
장소명 <somyeong3@naver.com>
2022-05-09 17:15:12 +0900

    [S06P31C107-324] fix: 메세지 가져오기 수정

--- proma/src/main/java/com/ssafy/proma/repository/chat/PrivateChatMessageRepository.java
+++ proma/src/main/java/com/ssafy/proma/repository/chat/PrivateChatMessageRepository.java
@@ -3,13 +3,12 @@ package com.ssafy.proma.repository.chat;
 
 import com.ssafy.proma.model.entity.chat.PrivateChatMessage;
 import com.ssafy.proma.model.entity.chat.PrivateChatRoom;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PrivateChatMessageRepository extends JpaRepository<PrivateChatMessage, Integer> {
 
-  Page<List<PrivateChatMessage>> findByChatRoomOrderByTimeDesc(PrivateChatRoom privateChatRoom, Pageable pageable);
+  Page<PrivateChatMessage> findByChatRoomOrderByTimeDesc(PrivateChatRoom privateChatRoom, Pageable pageable);
 
 }
--- proma/src/main/java/com/ssafy/proma/repository/chat/TeamChatMessageRepository.java
+++ proma/src/main/java/com/ssafy/proma/repository/chat/TeamChatMessageRepository.java
@@ -3,13 +3,12 @@ package com.ssafy.proma.repository.chat;
 
 import com.ssafy.proma.model.entity.chat.TeamChatMessage;
 import com.ssafy.proma.model.entity.chat.TeamChatRoom;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamChatMessageRepository extends JpaRepository<TeamChatMessage, Integer> {
 
-  Page<List<TeamChatMessage>> findByChatRoomOrderByTimeDesc(TeamChatRoom teamChatRoom, Pageable pageable);
+  Page<TeamChatMessage> findByChatRoomOrderByTimeDesc(TeamChatRoom teamChatRoom, Pageable pageable);
 
 }
