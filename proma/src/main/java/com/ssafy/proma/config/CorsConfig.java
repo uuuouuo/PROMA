3e54dc6560319de39c317e67cf6475d19e893b1b
김일환 <ikhwan62@naver.com>
2022-05-11 17:26:35 +0900

    [S06P31C107] fix: JWT header 에러 수정

--- proma/src/main/java/com/ssafy/proma/config/CorsConfig.java
+++ proma/src/main/java/com/ssafy/proma/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.proma.config;
 
+import com.ssafy.proma.config.auth.jwt.JwtProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,8 +18,10 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOriginPattern("*");
         config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type","Refresh"));
+        config.setAllowedHeaders(Arrays.asList(JwtProperties.JWT_HEADER_STRING, "Cache-Control", "Content-Type", JwtProperties.REF_HEADER_STRING));
         config.setAllowCredentials(true);
+        config.addExposedHeader(JwtProperties.JWT_HEADER_STRING);
+        config.addExposedHeader(JwtProperties.REF_HEADER_STRING);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
